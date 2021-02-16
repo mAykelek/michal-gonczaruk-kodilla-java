3a8d0811c54012fce0dc8f2d194d313a1be887b0
michal gonczaruk <michal.business@o2.pl>
2021-02-16 04:42:57 +0100

    First commit

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.*;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -37,6 +38,9 @@ public class StreamMain {
         poemBeautifier2.beautify2(4, CreativeBeauties::creativeBeautify);
         poemBeautifier3.beautify3("Henryk Sienkiewicz", AuthorBeauty::authorBeauty);
         poemBeautifier.beautify("tralalala", "x4", Beauties::ZxcAndLol);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
 
     }
